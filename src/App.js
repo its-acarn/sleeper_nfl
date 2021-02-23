6d28fec7cdc2e840e8a83433640c15961cb899a3
its-acarn <andreww92@live.com>
2021-02-23 17:41:04 +0000

    added Roster fetch

--- src/App.js
+++ src/App.js
@@ -2,49 +2,78 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+	const [leagueID, setLeagueID] = useState('');
+	const [leagueUsers, setLeagueUsers] = useState([]);
+	const [leagueRosters, setLeagueRosters] = useState([]);
 
-  const [leagueID, setLeagueID] = useState('');
-  const [leagueUsers, setLeagueUsers] = useState(['football', 'choco']);
+	async function fetchLeagueUsers() {
+		try {
+			const res = await fetch(
+				`https://api.sleeper.app/v1/league/${leagueID}/users`
+			);
 
-  async function fetchLeagueUsers() {
-    try {
-      const res = await fetch(`https://api.sleeper.app/v1/league/${leagueID}/users`); 
+			const data = await res.json();
+			console.log(data);
+			setLeagueUsers(data);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-      const data = await res.json();
-      console.log(data);
-      setLeagueUsers(data);
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
+	async function fetchLeagueRosters() {
+		try {
+			const res = await fetch(
+				`https://api.sleeper.app/v1/league/${leagueID}/rosters`
+			);
 
-  const handleChange = (event) => {
-    setLeagueID(event.target.value);
-  }
+			const data = await res.json();
+			console.log(data);
+			setLeagueRosters(data);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    fetchLeagueUsers();
-    } 
-  
-  return (
-    <main className="App">
-      <section>
-        <form onSubmit={handleSubmit}>
-          <input type="text" value={leagueID} onChange={handleChange}/>
-          <input type="submit" value="Show League Users"/>
-        </form>
-      </section>
-      <section>
-        <h1>League Users</h1>
-        <div>
-          {leagueUsers.map((item, i) => {
-      return <p key={i}>{item.display_name}</p>})}
-        </div>
-      </section>
-    </main>
-  );
+	const handleChange = (event) => {
+		setLeagueID(event.target.value);
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		fetchLeagueUsers();
+		fetchLeagueRosters();
+	};
+
+	return (
+		<main className="App">
+			<section>
+				<form onSubmit={handleSubmit}>
+					<input
+						type="text"
+						value={leagueID}
+						onChange={handleChange}
+					/>
+					<input type="submit" value="Show League Users" />
+				</form>
+			</section>
+			<section>
+				<h1>League Users</h1>
+				<div>
+					{leagueUsers.map((item, i) => {
+						return <p key={i}>{item.display_name}</p>;
+					})}
+				</div>
+			</section>
+			<section>
+				<h1>League Rosters</h1>
+				<div>
+					{leagueRosters.map((item, i) => {
+						return <p key={i}>{item.players}</p>;
+					})}
+				</div>
+			</section>
+		</main>
+	);
 }
 
 export default App;
